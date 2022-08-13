b0eb3cd3a1c560a3961d9ee391a294795a2b8c40
M1m1c <M1m1c@users.noreply.github.com>
2022-08-13 10:49:06 +0200

    changed core logging name

--- Sol/Source/Sol/Log.cpp
+++ Sol/Source/Sol/Log.cpp
@@ -10,7 +10,7 @@ namespace Sol
 	{
 		spdlog::set_pattern("%^[%T] %n: %v%$");
 
-		s_CoreLogger = spdlog::stdout_color_mt("SOL");
+		s_CoreLogger = spdlog::stdout_color_mt("SOL_ENGINE");
 		s_CoreLogger->set_level(spdlog::level::trace);
 
 		s_ClientLogger = spdlog::stdout_color_mt("APP");
