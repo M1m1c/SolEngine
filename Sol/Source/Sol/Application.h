1c6c9b3548807bce42da67ab9b38153622b0c324
M1m1c <M1m1c@users.noreply.github.com>
2022-08-25 23:47:10 +0200

    added ImGui Docking

--- Sol/Source/Sol/Application.h
+++ Sol/Source/Sol/Application.h
@@ -5,6 +5,7 @@
 #include "LayerStack.h"
 #include "Events/Event.h";
 #include "Sol/Events/ApplicationEvent.h"
+#include "Sol/ImGui/ImGuiLayer.h"
 
 namespace Sol 
 {
@@ -27,6 +28,7 @@ namespace Sol
 		bool OnWindowClose(WindowClosedEvent& e);
 
 		std::unique_ptr<Window> m_Window;
+		ImGuiLayer* m_ImGuiLayer;
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
--- Sol/Source/Sol/LayerStack.cpp
+++ Sol/Source/Sol/LayerStack.cpp
@@ -6,7 +6,6 @@ namespace Sol
 {
 	LayerStack::LayerStack()
 	{
-		m_LayerInsert = m_Layers.begin();
 	}
 
 	LayerStack::~LayerStack()
@@ -19,7 +18,8 @@ namespace Sol
 
 	void LayerStack::PushLayer(Layer* layer)
 	{
-		m_LayerInsert = m_Layers.emplace(m_LayerInsert, layer);
+		m_Layers.emplace(m_Layers.begin() + m_LayerInsertIndex, layer);
+		m_LayerInsertIndex++;
 	}
 
 	void LayerStack::PushOverlay(Layer* overlay)
@@ -33,7 +33,7 @@ namespace Sol
 		if (it != m_Layers.end()) 
 		{
 			m_Layers.erase(it);
-			m_LayerInsert--;
+			m_LayerInsertIndex--;
 		}
 	}
 
