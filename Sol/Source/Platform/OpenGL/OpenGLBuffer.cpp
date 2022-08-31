89d24689578a9857bc8862c42b9f84a1277a2b37
M1m1c <M1m1c@users.noreply.github.com>
2022-08-31 18:31:19 +0200

    Abstracted vertex and index buffers to intrefaces

---
+++ Sol/Source/Platform/OpenGL/OpenGLBuffer.cpp
@@ -0,0 +1,58 @@
+#include "solpch.h"
+#include "OpenGLBuffer.h"
+
+#include <glad/glad.h>
+
+namespace Sol 
+{
+	//////////////////////////////////////////////////////////////////////////////
+	//VERTEX BUFFER
+	OpenGlVertexBuffer::OpenGlVertexBuffer(float* verts, uint32_t size)
+	{
+		glCreateBuffers(1, &m_RendererID);
+		glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
+		glBufferData(GL_ARRAY_BUFFER, size, verts, GL_STATIC_DRAW);
+	}
+
+	OpenGlVertexBuffer::~OpenGlVertexBuffer()
+	{
+		glDeleteBuffers(1, &m_RendererID);
+	}
+
+	void OpenGlVertexBuffer::Bind() const
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
+	}
+
+	void OpenGlVertexBuffer::UnBind() const
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	}
+
+	//////////////////////////////////////////////////////////////////////////////
+	//INDEX BUFFER
+	OpenGlIndexBuffer::OpenGlIndexBuffer(uint32_t* indices, uint32_t count) : m_Count(count)
+	{
+		glCreateBuffers(1, &m_RendererID);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(uint32_t), indices, GL_STATIC_DRAW);
+		
+	}
+
+	OpenGlIndexBuffer::~OpenGlIndexBuffer()
+	{
+		glDeleteBuffers(1, &m_RendererID);
+	}
+
+	void OpenGlIndexBuffer::Bind() const
+	{
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID);
+	}
+
+	void OpenGlIndexBuffer::UnBind() const
+	{
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	}
+
+
+}
--- Sol/Source/Sol/Application.h
+++ Sol/Source/Sol/Application.h
@@ -9,6 +9,7 @@
 
 //TEMPORARY, WILL BE REMOVED LATER
 #include "Renderer/Shader.h"
+#include <Sol/Renderer/Buffer.h>
 
 namespace Sol 
 {
@@ -35,8 +36,10 @@ namespace Sol
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		unsigned int m_VertexArray;
 		std::unique_ptr<Shader> m_Shader;
+		std::unique_ptr<VertexBuffer> m_VertexBuffer;
+		std::unique_ptr<IndexBuffer> m_IndexBuffer;
 
 		static Application* s_Instance;
 	};
