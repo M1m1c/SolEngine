7c9c9a097c12433a7e5bb35fda5ef29fcd01bffa
M1m1c <M1m1c@users.noreply.github.com>
2022-09-01 17:04:03 +0200

    Create GalaxyDraw.h

---
+++ Sol/Source/GalaxyDraw/GalaxyDraw.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "shaderClass.h"
+#include "Buffer.h"
+
+// This file is meant to be the only file you need to include to use GalaxyDraw.
+// It should mostly contain typedefs and macros for ease of use outside of GalaxyDraw.
+/////////////////////////////////////////////////////
+
+#define GD_ GalaxyDraw
+
+
+//Defined in Buffer.h
+/////////////////////////////////////////////////////
+typedef GalaxyDraw::VertexBuffer GD_VBO;
+typedef GalaxyDraw::IndexBuffer GD_EBO;
+typedef GalaxyDraw::ShaderDataType GD_ShaderDataType;
+typedef GalaxyDraw::BufferLayout GD_BufferLayout;
+/////////////////////////////////////////////////////
