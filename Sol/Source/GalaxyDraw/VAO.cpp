84ea79ec41e335f5c48279f899ae278437d176e0
M1m1c <M1m1c@users.noreply.github.com>
2022-09-01 12:21:43 +0200

    Added to name space GalaxyDraw and removed macro calls

--- Sol/Source/GalaxyDraw/VAO.cpp
+++ Sol/Source/GalaxyDraw/VAO.cpp
@@ -2,44 +2,49 @@
 
 #include "VAO.h"
 
-// Constructor that generates a VAO ID
-VAO::VAO()
+namespace GalaxyDraw
 {
-	GLCall(glGenVertexArrays(1, &ID));
-}
 
-VAO::VAO(const unsigned int vaoID) : ID(vaoID)
-{
-}
+	// Constructor that generates a VAO ID
+	VAO::VAO()
+	{
+		GLCall(glGenVertexArrays(1, &ID));
+	}
 
-// Links a VBO to the VAO using a certain layout
-void VAO::LinkAttrib(VBO& VBO, GLuint layout, GLuint numComponents, GLenum type, GLsizeiptr stride, void* offset)
-{
-	VBO.Bind();
-	
-	// GL_FALSE here refers to normalization:
-	GLCall(glVertexAttribPointer(layout, numComponents, type, GL_FALSE, stride, offset));
-	// normalized (from docs)
-	// - For glVertexAttribPointer, specifies whether fixed - point data values should be normalized(GL_TRUE) or converted directly as fixed - point values(GL_FALSE) when they are accessed.
+	VAO::VAO(const unsigned int vaoID) : ID(vaoID)
+	{
+	}
 
-	GLCall(glEnableVertexAttribArray(layout));
-	VBO.Unbind();
-}
+	// Links a VBO to the VAO using a certain layout
+	void VAO::LinkAttrib(VBO& VBO, GLuint layout, GLuint numComponents, GLenum type, GLsizeiptr stride, void* offset)
+	{
+		VBO.Bind();
 
-// Binds the VAO
-void VAO::Bind() const
-{
-	GLCall(glBindVertexArray(ID));
-}
+		// GL_FALSE here refers to normalization:
+		GLCall(glVertexAttribPointer(layout, numComponents, type, GL_FALSE, stride, offset));
+		// normalized (from docs)
+		// - For glVertexAttribPointer, specifies whether fixed - point data values should be normalized(GL_TRUE) or converted directly as fixed - point values(GL_FALSE) when they are accessed.
 
-// Unbinds the VAO
-void VAO::Unbind() const
-{
-	GLCall(glBindVertexArray(0));
-}
+		GLCall(glEnableVertexAttribArray(layout));
+		VBO.Unbind();
+	}
 
-// Deletes the VAO
-void VAO::Delete()
-{
-	GLCall(glDeleteVertexArrays(1, &ID));
+	// Binds the VAO
+	void VAO::Bind() const
+	{
+		GLCall(glBindVertexArray(ID));
+	}
+
+	// Unbinds the VAO
+	void VAO::Unbind() const
+	{
+		GLCall(glBindVertexArray(0));
+	}
+
+	// Deletes the VAO
+	void VAO::Delete()
+	{
+		GLCall(glDeleteVertexArrays(1, &ID));
+	}
+
 }
--- Sol/Source/GalaxyDraw/VAO.h
+++ Sol/Source/GalaxyDraw/VAO.h
@@ -6,22 +6,27 @@
 #include "VBO.h"
 #include "GLMacros.h"
 
-class VAO
+namespace GalaxyDraw 
 {
-public:
-	// ID reference for the Vertex Array Object
-	GLuint ID;
-	// Constructor that generates a VAO ID
-	VAO();
-	VAO(const unsigned int vaoID);
 
-	// Links a VBO Attribute such as a position or color to the VAO
-	void LinkAttrib(VBO& VBO, GLuint layout, GLuint numComponents, GLenum type, GLsizeiptr stride, void* offset);
-	// Binds the VAO
-	void Bind() const;
-	// Unbinds the VAO
-	void Unbind() const;
-	// Deletes the VAO
-	void Delete();
-};
+	class VAO
+	{
+	public:
+		// ID reference for the Vertex Array Object
+		GLuint ID;
+		// Constructor that generates a VAO ID
+		VAO();
+		VAO(const unsigned int vaoID);
+
+		// Links a VBO Attribute such as a position or color to the VAO
+		void LinkAttrib(VBO& VBO, GLuint layout, GLuint numComponents, GLenum type, GLsizeiptr stride, void* offset);
+		// Binds the VAO
+		void Bind() const;
+		// Unbinds the VAO
+		void Unbind() const;
+		// Deletes the VAO
+		void Delete();
+	};
+
+}
 #endif#pragma once
