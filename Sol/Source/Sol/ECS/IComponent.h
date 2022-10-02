9d132d14b58b5d8ed61cb106f73a0f883900ac5a
M1m1c <M1m1c@users.noreply.github.com>
2022-10-02 17:28:12 +0200

    removed template from interface

--- Sol/Source/Sol/ECS/IComponent.h
+++ Sol/Source/Sol/ECS/IComponent.h
@@ -2,11 +2,10 @@
 #include "ECS_Core.h"
 namespace Sol
 {
-	template<typename T>
 	class IComponent
 	{
 	public:
-		virtual IComponent* Create(T startValue) = 0;// this should take in a parameter that is dependent on what component it is
+		virtual IComponent* Create() = 0;// this should take in a parameter that is dependent on what component it is
 		virtual ~IComponent() = default;
 		virtual const CompType& GetType() const = 0;
 		//virtual void EntityDestroyed(Entity entity) = 0;
