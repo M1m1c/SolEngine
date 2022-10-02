adf6bb4bc8094c660c1cae3b3ffb628ea3782775
M1m1c <M1m1c@users.noreply.github.com>
2022-10-02 17:11:12 +0200

    added virtual funcitons to ISystem

--- Sol/Source/Sol/ECS/ISystem.h
+++ Sol/Source/Sol/ECS/ISystem.h
@@ -1,17 +1,20 @@
 #pragma once
+#include "ECS_Core.h"
+#include "IComponent.h"
+#include <set>
 
 namespace Sol
 {
 	class ISystem
 	{
 	public:
-		ISystem();
-		~ISystem();
-
+		virtual ~ISystem() = default;
 		virtual void OnStartSystem() = 0;
 		virtual void OnUpdateSystem() = 0;
 
-	private:
+		virtual const std::set<Entity>& GetEntitiesSet() const = 0;
+		virtual void AddEntitity(const Entity entity) const = 0;
+		virtual void RemoveEntitity(const Entity entity) const = 0;
 
 	};
 
