3b65151b35fec6e55c30e75b4fd0c2a62c099be3
M1m1c <M1m1c@users.noreply.github.com>
2022-10-10 14:14:19 +0200

    Added start of particlesystem class

---
+++ Sol/Source/GalaxyDraw/Interfaces/ParticleSystem.cpp
@@ -0,0 +1,24 @@
+#include "solpch.h"
+#include "ParticleSystem.h"
+#include "Renderer.h"
+#include "GalaxyDraw/Platform/OpenGL/OpenGL_ParticleSystem.h"
+
+namespace GalaxyDraw 
+{
+    std::shared_ptr<ParticleSystem> GalaxyDraw::ParticleSystem::Create(const std::string& vertexFile, const std::string& fragmentFile)
+    {
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:
+			SOL_CORE_ASSERT(false, "RendererAPI::None is not supported!");
+			return nullptr;
+
+		case RendererAPI::API::OpenGL:
+				return std::make_shared<OpenGL_ParticleSystem>(vertexFile,fragmentFile);
+			break;
+		}
+		SOL_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+    }
+}
+
