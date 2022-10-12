d0227493a7cd450c6410446dc68372d9a0ede589
M1m1c <M1m1c@users.noreply.github.com>
2022-10-12 14:30:07 +0200

    changed to uint32_t

--- Sol/Source/Sol/Core/Window.h
+++ Sol/Source/Sol/Core/Window.h
@@ -11,12 +11,12 @@ namespace Sol
 	{
 		std::string Title;
 
-		unsigned int Width;
-		unsigned int Height;
+		uint32_t Width;
+		uint32_t Height;
 
 		WindowProps(const std::string title = "Sol Engine",
-			unsigned int width = 1200,
-			unsigned int height = 720)
+			uint32_t width = 1200,
+			uint32_t height = 720)
 			: Title(title), Width(width), Height(height){}
 	};
 
@@ -28,8 +28,8 @@ namespace Sol
 		virtual ~Window() {}
 		virtual void OnUpdate() = 0;
 
-		virtual unsigned int GetWidth() const = 0;
-		virtual unsigned int GetHeight() const = 0;
+		virtual uint32_t GetWidth() const = 0;
+		virtual uint32_t GetHeight() const = 0;
 
 		virtual void SetEventCallback(const EventCallbackFn& callback) = 0;
 		virtual void SetVSync(bool enabled) = 0;
