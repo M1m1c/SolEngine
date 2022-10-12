dc57ea69f0abdd6c2401583416b2026889e16899
M1m1c <M1m1c@users.noreply.github.com>
2022-10-12 14:31:35 +0200

    changed from .h to .cpp

--- Sol-Editor/Source/SolEditorApp.cpp
+++ Sol-Editor/Source/SolEditorApp.cpp
@@ -1,12 +1,14 @@
 #include "Sol.h"
 #include "Sol/Core/EntryPoint.h"
+
 #include "EditorLayer.h"
 namespace Sol 
 {
 	class SolEditorApp : public Application
 	{
 	public:
-		SolEditorApp()
+		SolEditorApp() : 
+			Application("Sol-Editor")
 		{
 			PushLayer(new EditorLayer());
 		}
