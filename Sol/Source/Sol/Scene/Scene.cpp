8ac570f7c40256e9e3f7cc299ce7e44df5b256e1
M1m1c <M1m1c@users.noreply.github.com>
2022-11-21 18:35:26 +0100

    removed manual drawing call used for testing

--- Sol/Source/Sol/Scene/Scene.cpp
+++ Sol/Source/Sol/Scene/Scene.cpp
@@ -8,44 +8,6 @@ namespace Sol
 {
 	Scene::Scene()
 	{
-		m_VertexArray = GD_VAO::Create();
-
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			0.5f, -0.5f, 0.0f,
-			0.0f, 0.5f, 0.0f
-		};
-
-		float squareVertices[5 * 4] = {
-			-0.5f, -0.5f, 0.0f, 0.f, 0.f,
-			0.5f, -0.5f, 0.0f, 1.f, 0.f,
-			0.5f, 0.5f, 0.0f, 1.f, 1.f,
-			-0.5f, 0.5f, 0.0f, 0.f, 1.f,
-		};
-
-		auto vertexBuffer = GD_VBO::Create(squareVertices, sizeof(squareVertices));
-
-		GD_BufferLayout layout =
-		{
-			{GD_ShaderDataType::Float3, "a_Position"},
-			{GD_ShaderDataType::Float2, "a_TexCoord"},
-			/*	{GD_ShaderDataType::Float3, "a_Normal"},
-				{GD_ShaderDataType::Float4, "a_Color"},*/
-		};
-
-		vertexBuffer->SetLayout(layout);
-		m_VertexArray->AddVertexBuffer(vertexBuffer);
-
-		uint32_t indices[6] = { 0,1,2,2,3,0 };
-		auto indexBuffer = GD_EBO::Create(indices, sizeof(indices) / sizeof(uint32_t));
-		m_VertexArray->SetIndexBuffer(indexBuffer);
-
-		auto shader = m_ShaderLib.Load("Square", "Square.vert", "Square.frag");
-
-		m_Texture = GD_Texture2D::Create("assets/textures/think.png");
-
-		shader->Bind();
-		shader->setInt("u_Texture", 0);
 	}
 
 	Scene::~Scene()
@@ -75,8 +37,6 @@ namespace Sol
 
 		if (mainCamera)
 		{
-			//TODO begining a scene here with this camera is unable to see teh smiley, investigate why
-
 			GD_Renderer::BeginScene(mainCamera->GetProjection(), *cameraTransform);
 
 			auto group = m_Registry.group<TransformComp>(entt::get<SpriteRendererComp>);
@@ -84,20 +44,8 @@ namespace Sol
 			{
 				auto& [transform, sprite] = group.get<TransformComp, SpriteRendererComp>(entity);
 
-				//TODO Render stuff
-				/*glm::vec3 pos(0.f, 0.f, 0.f);
-				glm::mat4 transform2 = glm::translate(glm::mat4(1.0f), pos);*/
-
 				GD_Renderer::DrawQuad(transform, sprite.Color);
 			}
-
-			
-
-			/*auto shader = m_ShaderLib.Get("Square");
-
-			GD_Renderer::Submit(shader, m_VertexArray, transform);
-
-			m_Texture->Bind();*/
 
 			GD_Renderer::EndScene();
 		}
--- Sol/Source/Sol/Scene/Scene.h
+++ Sol/Source/Sol/Scene/Scene.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <entt.hpp>
 #include <Sol/Core/TimeStep.h>
-#include "GalaxyDraw/GalaxyDraw.h"
 
 namespace Sol
 {
@@ -28,11 +27,6 @@ namespace Sol
 		uint32_t m_ViewportHeight = 0;
 
 		friend class Entity;
-
-		GD_ShaderLibrary m_ShaderLib;
-		s_ptr<GD_Shader> m_Shader;
-		s_ptr<GD_VAO> m_VertexArray;
-		s_ptr<GD_Texture2D> m_Texture;
 	};
 
 }
