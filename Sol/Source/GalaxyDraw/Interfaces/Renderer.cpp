e14fe6342e37586380bf9621939146cd7bd824b2
M1m1c <M1m1c@users.noreply.github.com>
2022-11-25 17:10:41 +0100

    Added Renderer3DData

--- Sol/Source/GalaxyDraw/Interfaces/Renderer.cpp
+++ Sol/Source/GalaxyDraw/Interfaces/Renderer.cpp
@@ -2,10 +2,37 @@
 #include "Renderer.h"
 #include "Renderer2D.h"
 #include "RenderCommand.h"
+#include <GalaxyDraw/Interfaces/UniformBuffer.h>
 
 namespace GalaxyDraw {
 
+	struct Renderer3DData
+	{
+		static const uint32_t MaxMeshes = 20000;
+		static const uint32_t MaxVertices = MaxMeshes * 4;
+		static const uint32_t MaxIndices = MaxMeshes * 6;
+		static const uint32_t MaxTextureSlots = 32; // TODO: RenderCaps
+
+		std::shared_ptr<VertexArray> MeshVertexArray;
+		std::shared_ptr<VertexBuffer> MeshVertexBuffer;
+		std::shared_ptr<Shader> MeshShader;
+		std::shared_ptr<Texture2D> MissingTexture;
+
+		uint32_t MeshIndexCount = 0;
+		Vertex* MeshVertexBufferBase = nullptr;
+		Vertex* MeshVertexBufferPtr = nullptr;
+
+		struct CameraData
+		{
+			glm::mat4 ViewProjection;
+		};
+		CameraData CameraBuffer;
+		std::shared_ptr<UniformBuffer> CameraUniformBuffer;
+	};
+
 	std::unique_ptr<Renderer::SceneData> Renderer::s_SceneData = std::make_unique<Renderer::SceneData>();
+
+	static Renderer3DData s_Data;
 
 	void Renderer::Init()
 	{
