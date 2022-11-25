fdd29de0917198672686ae2da470359452675384
M1m1c <M1m1c@users.noreply.github.com>
2022-11-25 17:00:22 +0100

    Implemented part of assimp model loading from ModelOld

--- Sol/Source/GalaxyDraw/Platform/OpenGL/OpenGL_Model.cpp
+++ Sol/Source/GalaxyDraw/Platform/OpenGL/OpenGL_Model.cpp
@@ -1,24 +1,120 @@
 #include "solpch.h"
 #include "OpenGL_Model.h"
 
+#include "GalaxyDraw/Platform/OpenGL/GLMacros.h"
+#include <glad/glad.h>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 namespace GalaxyDraw
 {
     OpenGL_Model::OpenGL_Model(const std::string& modelpath)
     {
+        LoadModel(modelpath);
     }
 
     void OpenGL_Model::SetData(const std::string& path)
     {
+        LoadModel(path);
     }
 
     bool OpenGL_Model::operator==(const Model& other) const
     {
-        return false;
+        return m_RendererID == ((OpenGL_Model&)other).m_RendererID;
     }
 
-    std::vector<Mesh> OpenGL_Model::GetMeshes() const
+    void OpenGL_Model::LoadModel(const std::string& modelpath)
     {
-        return std::vector<Mesh>();
+        Assimp::Importer import;
+        const aiScene* scene = import.ReadFile(modelpath, aiProcess_Triangulate | aiProcess_FlipUVs);
+
+        if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+        {
+            std::cout << "ERROR::ASSIMP::" << import.GetErrorString() << std::endl;
+            return;
+        }
+        m_ModelDirectory = modelpath.substr(0, modelpath.find_last_of('/'));
+
+        ProcessNode(scene->mRootNode, scene);
+    }
+
+    void OpenGL_Model::ProcessNode(aiNode* node, const aiScene* scene)
+    {
+        // process all the node's meshes (if any)
+        for (unsigned int i = 0; i < node->mNumMeshes; i++)
+        {
+            aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
+            m_Meshes.push_back(ProcessMesh(mesh, scene));
+        }
+        // then do the same for each of its children
+        for (unsigned int i = 0; i < node->mNumChildren; i++)
+        {
+            ProcessNode(node->mChildren[i], scene);
+        }
+    }
+
+    Mesh OpenGL_Model::ProcessMesh(aiMesh* mesh, const aiScene* scene)
+    {
+		std::vector<Vertex> vertices;
+		std::vector<uint32_t> indices;
+		std::vector<std::shared_ptr<Texture>> textures;
+
+		for (unsigned int i = 0; i < mesh->mNumVertices; i++)
+		{
+			Vertex vertex;
+
+			glm::vec3 vector;
+			vector.x = mesh->mVertices[i].x;
+			vector.y = mesh->mVertices[i].y;
+			vector.z = mesh->mVertices[i].z;
+			vertex.Position = vector;
+
+			vector.x = mesh->mNormals[i].x;
+			vector.y = mesh->mNormals[i].y;
+			vector.z = mesh->mNormals[i].z;
+			vertex.Normal = vector;
+
+			// does the mesh contain texture coordinates?
+			if (mesh->mTextureCoords[0])
+			{
+				glm::vec2 vec;
+				vec.x = mesh->mTextureCoords[0][i].x;
+				vec.y = mesh->mTextureCoords[0][i].y;
+				vertex.TexCoords = vec;
+			}
+			else
+			{
+				vertex.TexCoords = glm::vec2(0.0f, 0.0f);
+			}
+
+			vertices.push_back(vertex);
+		}
+
+		// LMAO, I wish C# procedural shit went like this
+		for (unsigned int i = 0; i < mesh->mNumFaces; i++)
+		{
+			aiFace face = mesh->mFaces[i];
+			for (unsigned int j = 0; j < face.mNumIndices; j++)
+			{
+				indices.push_back(face.mIndices[j]);
+			}
+		}
+
+		// Checks if any material exists at all
+		//if (mesh->mMaterialIndex >= 0)
+		//{
+		//	// gets the material cached within that specific index
+		//	aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
+
+		//	std::vector<Texture> diffuseMaps = loadMaterialTextures(material, aiTextureType_DIFFUSE, "texture_diffuse");
+		//	textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
+
+		//	std::vector<Texture> specularMaps = loadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular");
+		//	textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
+		//}
+
+		return Mesh(vertices, indices, textures);
     }
 
 }
--- Sol/Source/GalaxyDraw/Platform/OpenGL/OpenGL_Model.h
+++ Sol/Source/GalaxyDraw/Platform/OpenGL/OpenGL_Model.h
@@ -1,5 +1,10 @@
 #pragma once
 #include "GalaxyDraw/Interfaces/Model.h"
+
+struct aiNode;
+struct aiScene;
+struct aiMesh;
+
 namespace GalaxyDraw 
 {
 	class OpenGL_Model : public Model
@@ -8,12 +13,24 @@ namespace GalaxyDraw
 		OpenGL_Model(const std::string& modelpath);
 
 		// Inherited via Model3D
+
 		virtual void SetData(const std::string& path) override;
 
 		virtual bool operator==(const Model& other) const override;
 
-		virtual std::vector<Mesh> GetMeshes() const override;
+		virtual uint32_t GetRendererID() const override { return m_RendererID; };
 
+		virtual std::vector<Mesh>& GetMeshes() override { return m_Meshes; };
+
+	private:
+		void LoadModel(const std::string& modelpath);
+		void ProcessNode(aiNode* node, const aiScene* scene);
+		Mesh ProcessMesh(aiMesh* mesh, const aiScene* scene);
+
+		uint32_t m_RendererID;
+		std::string m_ModelDirectory;
+
+		std::vector<Mesh> m_Meshes;
 	};
 
 	
