c9022a84a299de523f644086632a293b80267a65
M1m1c <M1m1c@users.noreply.github.com>
2022-12-30 15:10:31 +0100

    updated to use vertex attribute specs to determine VertexAttribPointer

--- Sol/Source/GalaxyDraw/Platform/OpenGL/OpenGL_InstanceBuffer.h
+++ Sol/Source/GalaxyDraw/Platform/OpenGL/OpenGL_InstanceBuffer.h
@@ -10,22 +10,21 @@ namespace GalaxyDraw
 	class OpenGL_InstanceBuffer : public InstanceBuffer
 	{
 	public:
-		OpenGL_InstanceBuffer(GLsizeiptr size);
+		OpenGL_InstanceBuffer(GLsizeiptr size, uint32_t stride, std::vector<VertexAttributeSpecs> specs);
 		~OpenGL_InstanceBuffer();
 
 
-		// Inherited via InstanceBuffer
 		virtual void Bind() const override;
 
 		virtual void Unbind() const override;
 
-
-		// Inherited via InstanceBuffer
 		virtual void SetData(const void* data, uint32_t size) override;
 
 	private:
 		// Reference ID of the Vertex Buffer Object
 		GLuint ID;
+
+		void SetVertexAttributes(uint32_t stride, std::vector<VertexAttributeSpecs> specs);
 	};
 
 	
