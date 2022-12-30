0fc2a42a85ac77bbb2fdea2e42a47ceb13a83224
M1m1c <M1m1c@users.noreply.github.com>
2022-12-30 15:09:50 +0100

    added vertex attibute specs struct

--- Sol/Source/GalaxyDraw/Interfaces/Buffer.cpp
+++ Sol/Source/GalaxyDraw/Interfaces/Buffer.cpp
@@ -65,7 +65,7 @@ namespace GalaxyDraw
 	}
 
 
-	std::shared_ptr<InstanceBuffer> InstanceBuffer::Create(uint32_t size)
+	std::shared_ptr<InstanceBuffer> InstanceBuffer::Create(uint32_t size, uint32_t stride, std::vector<VertexAttributeSpecs> specs)
 	{
 		switch (Renderer::GetAPI())
 		{
@@ -73,7 +73,7 @@ namespace GalaxyDraw
 			SOL_CORE_ASSERT(false, "RendererAPI::None is not supported!");
 			return nullptr;
 
-		case RendererAPI::API::OpenGL: return std::make_shared<OpenGL_InstanceBuffer>(size);
+		case RendererAPI::API::OpenGL: return std::make_shared<OpenGL_InstanceBuffer>(size,stride,specs);
 		}
 		SOL_CORE_ASSERT(false, "Unknown RendererAPI!");
 		return nullptr;
