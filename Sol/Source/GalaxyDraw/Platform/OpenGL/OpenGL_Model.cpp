600d8039fb0f5e8e624ea8be5a9918999374d77b
M1m1c <M1m1c@users.noreply.github.com>
2023-02-14 15:34:44 +0100

    Fixed formatting

--- Sol/Source/GalaxyDraw/Platform/OpenGL/OpenGL_Model.cpp
+++ Sol/Source/GalaxyDraw/Platform/OpenGL/OpenGL_Model.cpp
@@ -9,60 +9,60 @@
 
 namespace GalaxyDraw
 {
-    OpenGL_Model::OpenGL_Model(const std::string& modelpath)
-    {
-        LoadModel(modelpath);
-    }
+	OpenGL_Model::OpenGL_Model(const std::string& modelpath)
+	{
+		LoadModel(modelpath);
+	}
 
-    void OpenGL_Model::SetData(const std::string& path)
-    {
-        LoadModel(path);
-    }
+	void OpenGL_Model::SetData(const std::string& path)
+	{
+		LoadModel(path);
+	}
 
-    bool OpenGL_Model::operator==(const Model& other) const
-    {
-        return m_RendererID == ((OpenGL_Model&)other).m_RendererID;
-    }
+	bool OpenGL_Model::operator==(const Model& other) const
+	{
+		return m_RendererID == ((OpenGL_Model&)other).m_RendererID;
+	}
 
-    void OpenGL_Model::LoadModel(const std::string& modelpath)
-    {
-        Assimp::Importer import;
-        const aiScene* scene = import.ReadFile(modelpath, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_GenNormals | aiProcess_JoinIdenticalVertices);
+	void OpenGL_Model::LoadModel(const std::string& modelpath)
+	{
+		Assimp::Importer import;
+		const aiScene* scene = import.ReadFile(modelpath, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_GenNormals | aiProcess_JoinIdenticalVertices);
 
-        if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
-        {
-            std::cout << "ERROR::ASSIMP::" << import.GetErrorString() << std::endl;
-            return;
-        }
-        m_ModelDirectory = modelpath.substr(0, modelpath.find_last_of('/'));
+		if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+		{
+			std::cout << "ERROR::ASSIMP::" << import.GetErrorString() << std::endl;
+			return;
+		}
+		m_ModelDirectory = modelpath.substr(0, modelpath.find_last_of('/'));
 
 		m_Name = modelpath.substr(modelpath.find_last_of('/') + 1);
 
-        ProcessNode(scene->mRootNode, scene);
-    }
+		ProcessNode(scene->mRootNode, scene);
+	}
 
-    void OpenGL_Model::ProcessNode(aiNode* node, const aiScene* scene)
-    {
+	void OpenGL_Model::ProcessNode(aiNode* node, const aiScene* scene)
+	{
 		//node->mt
-        // process all the node's meshes (if any)
-        for (unsigned int i = 0; i < node->mNumMeshes; i++)
-        {
-            aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
-            m_Meshes.push_back(ProcessMesh(mesh, scene, node));
-        }
-        // then do the same for each of its children
-        for (unsigned int i = 0; i < node->mNumChildren; i++)
-        {
-            ProcessNode(node->mChildren[i], scene);
-        }
-    }
+		// process all the node's meshes (if any)
+		for (unsigned int i = 0; i < node->mNumMeshes; i++)
+		{
+			aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
+			m_Meshes.push_back(ProcessMesh(mesh, scene, node));
+		}
+		// then do the same for each of its children
+		for (unsigned int i = 0; i < node->mNumChildren; i++)
+		{
+			ProcessNode(node->mChildren[i], scene);
+		}
+	}
 
-    std::shared_ptr<Mesh> OpenGL_Model::ProcessMesh(aiMesh* mesh, const aiScene* scene, aiNode* node)
-    {
+	std::shared_ptr<Mesh> OpenGL_Model::ProcessMesh(aiMesh* mesh, const aiScene* scene, aiNode* node)
+	{
 		std::vector<Vertex> vertices;
 		std::vector<uint32_t> indices;
 		std::vector<std::shared_ptr<Texture>> textures;
-	
+
 
 		for (unsigned int i = 0; i < mesh->mNumVertices; i++)
 		{
@@ -117,22 +117,20 @@ namespace GalaxyDraw
 		//	std::vector<Texture> specularMaps = loadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular");
 		//	textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
 		//}
+
 		aiVector3D scale;
 		aiVector3D rotation;
 		aiVector3D position;
 		node->mTransformation.Decompose(scale, rotation, position);
-		
-		glm::vec3 meshScale = glm::vec3(scale.x, scale.y, scale.z);
+
+		glm::vec3 meshScale = glm::vec3(scale.x, scale.y, scale.z) * 0.01f;
 		glm::vec3 meshRotation = glm::vec3(rotation.x, rotation.y, rotation.z);
 		glm::vec3 meshPosition = glm::vec3(position.x, position.y, position.z);
 
-		return  std::make_shared<Mesh>(
-			mesh->mName.C_Str(),
-			vertices,
-			indices, 
-			textures,
-			Sol::TransformComp(meshPosition,meshRotation,meshScale));
-    }
+		auto transform = Sol::TransformComp(meshPosition, meshRotation, meshScale);
+
+		return  std::make_shared<Mesh>(mesh->mName.C_Str(), vertices, indices, textures, transform);
+	}
 
 }
 
