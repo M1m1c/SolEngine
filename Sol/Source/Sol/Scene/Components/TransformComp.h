93d8b5a95cdd27e15d6a3a9addbcc648b092b28e
M1m1c <M1m1c@users.noreply.github.com>
2023-03-01 17:32:12 +0100

    added get forward, right and up funcitons to transform

--- Sol/Source/Sol/Scene/Components/TransformComp.h
+++ Sol/Source/Sol/Scene/Components/TransformComp.h
@@ -24,10 +24,61 @@ namespace Sol
 				* glm::scale(glm::mat4(1.0f), Scale);
 		}
 
+		glm::vec3 GetForward()
+		{
+			glm::vec3 dir;
+			float pitch = glm::radians(Pitch());
+			float yaw = glm::radians(Yaw());
+
+			dir.x = -cos(yaw) * sin(pitch);
+			dir.y = sin(yaw) * sin(pitch);
+			dir.z = cos(pitch);
+
+			return glm::normalize(dir);
+		}
+
+		glm::vec3 GetUp()
+		{
+			glm::vec3 up;
+			float pitch = glm::radians(Pitch());
+			float yaw = glm::radians(Yaw());
+			float roll = glm::radians(Roll());
+
+			up.x = sin(pitch) * sin(roll) * cos(yaw) + cos(pitch) * sin(yaw);
+			up.y = sin(pitch) * sin(roll) * sin(yaw) - cos(pitch) * cos(yaw);
+			up.z = cos(pitch) * cos(roll);
+
+			return glm::normalize(up);
+		}
+
+		glm::vec3 GetRight()
+		{
+			glm::vec3 right;
+			float yaw = glm::radians(Yaw());
+			float roll = glm::radians(Roll());
+
+			right.x = cos(roll) * cos(yaw);
+			right.y = cos(roll) * sin(yaw);
+			right.z = -sin(roll);
+
+			return glm::normalize(right);
+		}
+
 		operator const glm::mat4() const {
 			return GetTransformMatrix();
 		}
 
+		float& Pitch() {
+			return Rotation.y;
+		}
+
+		float& Yaw() {
+			return Rotation.x;
+		}
+
+		float& Roll() {
+			return Rotation.z;
+		}
 
 		glm::vec3 Position = { 0.0f, 0.0f, 0.0f };
 		glm::vec3 Rotation = { 0.0f, 0.0f, 0.0f };
