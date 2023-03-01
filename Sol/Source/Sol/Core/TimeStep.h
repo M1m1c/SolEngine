1b36450a1653f2e811b15acfcea3b3a4f1c02e70
M1m1c <M1m1c@users.noreply.github.com>
2023-03-01 19:06:50 +0100

    added more operator overloads

--- Sol/Source/Sol/Core/TimeStep.h
+++ Sol/Source/Sol/Core/TimeStep.h
@@ -22,8 +22,21 @@ namespace Sol
 			return *this;
 		}
 
+		TimeStep operator+(const float& addative) const {
+			return TimeStep(m_Time + addative);
+		}
+
+		TimeStep operator-(const float& subtractor) const {
+			return TimeStep(m_Time - subtractor);
+		}
+
+		TimeStep operator*(const float& multiplier) const {
+			return TimeStep(m_Time * multiplier);
+		}
+
 		float GetMilliSeconds() const { return m_Time * 1000.f; }
 	private:
 		float m_Time;
 	};
+
 }
