16fb147af6b94191a25e6862f50bd4c9e30d64f4
M1m1c <M1m1c@users.noreply.github.com>
2023-03-06 11:14:52 +0100

    added getname function

--- Sol/Source/Sol/Scene/Components/NameComp.h
+++ Sol/Source/Sol/Scene/Components/NameComp.h
@@ -11,6 +11,7 @@ namespace Sol
 		NameComp(const std::string& name) :m_Name(name) {}
 		~NameComp() = default;
 
+		std::string GetName() { return m_Name; }
 		operator std::string& () { return m_Name; }
 		operator const std::string& () const { return m_Name; }
 	private:
