bd3012a7003bf774c49a550f7421fa7398a61841
M1m1c <M1m1c@users.noreply.github.com>
2023-03-06 14:59:44 +0100

    made SceneHierarchy use properties panel

--- Sol-Editor/Source/Panels/SceneHierarchyPanel.cpp
+++ Sol-Editor/Source/Panels/SceneHierarchyPanel.cpp
@@ -10,51 +10,54 @@ namespace Sol
 		SetCurrentScene(scene);
 	}
 
-	SceneHierarchyPanel::~SceneHierarchyPanel()
-	{
-	}
-
-	void SceneHierarchyPanel::SetCurrentScene(const s_ptr<Scene>& scene)
-	{
-		m_CurrentScene = scene;
-	}
-
+	
 	void SceneHierarchyPanel::OnImGuiRender()
 	{
+		bool hasActiveScene = m_CurrentScene != nullptr;
+		
 		ImGui::Begin("Scene Hierarchy");
-		auto& sceneRegistry = m_CurrentScene->GetRegistry();
+		if (hasActiveScene) 
+		{
+			auto& sceneRegistry = m_CurrentScene->GetRegistry();
 
-		sceneRegistry.each([&](auto entityID)
-			{
-
-				Entity entity{ entityID, m_CurrentScene.get() };
-				bool shouldShow = true;
-
-				auto isInternal = entity.HasComponent<InternalComp>();
-				if (isInternal)
+			sceneRegistry.each([&](auto entityID)
 				{
-					shouldShow = entity.GetComponent<InternalComp>().IsVisible();
-				}
 
-				if (shouldShow)
-				{
-					DrawEntityNode(entity);
-				}
+					Entity entity{ entityID, m_CurrentScene.get() };
+					bool shouldShow = true;
+
+					auto isInternal = entity.HasComponent<InternalComp>();
+					if (isInternal)
+					{
+						shouldShow = entity.GetComponent<InternalComp>().IsVisible();
+					}
+
+					if (shouldShow)
+					{
+						DrawEntityNode(entity);
+					}
 
 
-			});
-
+				});
+		}	
 		ImGui::End();
+
+		if (hasActiveScene && m_PropertiesPanel != nullptr && m_CurrentSelection != m_OldSelection)
+		{
+			m_OldSelection = m_CurrentSelection;
+			m_PropertiesPanel->SetCurrentSelection(m_CurrentSelection);
+		}
 	}
 
 	void SceneHierarchyPanel::DrawEntityNode(Entity entity)
 	{
-		auto name = entity.GetComponent<NameComp>().GetName();
+		auto& name = entity.GetComponent<NameComp>().GetName();
 		ImGuiTreeNodeFlags flags =
 			(m_CurrentSelection == entity ? ImGuiTreeNodeFlags_Selected : 0) |
 			ImGuiTreeNodeFlags_OpenOnArrow;
 
 		bool opened = ImGui::TreeNodeEx((void*)(uint64_t)(uint32_t)entity, flags, name.c_str());
+
 		if (ImGui::IsItemClicked())
 		{
 			m_CurrentSelection = entity;
--- Sol-Editor/Source/Panels/SceneHierarchyPanel.h
+++ Sol-Editor/Source/Panels/SceneHierarchyPanel.h
@@ -4,15 +4,18 @@
 #include "Sol/Scene/Scene.h"
 #include "Sol/Scene/Entity.h"
 
+#include "PropertiesPanel.h"
+
 namespace Sol {
 	class SceneHierarchyPanel
 	{
 	public:
 		SceneHierarchyPanel() = default;
 		SceneHierarchyPanel(const s_ptr<Scene>& scene);
-		~SceneHierarchyPanel();
+		~SceneHierarchyPanel() = default;
 
-		void SetCurrentScene(const s_ptr<Scene>& scene);
+		void SetCurrentScene(const s_ptr<Scene>& scene) { m_CurrentScene = scene; }
+		void SetPropertiesPanel(PropertiesPanel* panel) { m_PropertiesPanel = panel; }
 
 		void OnImGuiRender();
 
@@ -22,6 +25,8 @@ namespace Sol {
 	private:
 
 		s_ptr<Scene> m_CurrentScene;
+		Entity m_OldSelection;
 		Entity m_CurrentSelection;
+		PropertiesPanel* m_PropertiesPanel;
 	};
 }
