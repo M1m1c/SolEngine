c9e43023513535b9183b9a3fb2024c274ad6d169
M1m1c <M1m1c@users.noreply.github.com>
2023-03-07 11:38:58 +0100

    added function for styling and drawing vec3 properties

--- Sol-Editor/Source/Panels/PropertiesPanel.cpp
+++ Sol-Editor/Source/Panels/PropertiesPanel.cpp
@@ -4,8 +4,73 @@
 
 #include <glm/gtc/type_ptr.hpp>
 #include <imgui.h>
+#include <imgui_internal.h>
 
 namespace Sol {
+
+	static void DrawVec3Control(const std::string& label, glm::vec3& values, float resetValue = 0.0f, float columnWidth = 80.f)
+	{
+		ImGui::PushID(label.c_str());
+		ImGui::Columns(2);
+		ImGui::SetColumnWidth(0, columnWidth);
+		ImGui::Text(label.c_str());
+		ImGui::NextColumn();
+
+		ImGui::PushMultiItemsWidths(3, ImGui::CalcItemWidth());
+		ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2{ 0,0 });
+		float lineHeight = GImGui->Font->FontSize + GImGui->Style.FramePadding.y * 2.f;
+
+		ImVec2 buttonSize = { lineHeight + 3.f, lineHeight };
+
+		{
+			ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.9f,0.2f,0.15f,1.f });
+			ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 1.f,0.2f,0.2f,1.f });
+			ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.9f,0.2f,0.15f,1.f });
+
+			if (ImGui::Button("X", buttonSize))
+				values.x = resetValue;
+			ImGui::PopStyleColor(3);
+
+			ImGui::SameLine();
+			ImGui::DragFloat("##X", &values.x, 0.1f);
+			ImGui::PopItemWidth();
+			ImGui::SameLine();
+		}
+
+		{
+			ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.2f,0.7f,0.15f,1.f });
+			ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.2f,0.8f,0.2f,1.f });
+			ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.2f,0.7f,0.15f,1.f });
+
+			if (ImGui::Button("Y", buttonSize))
+				values.y = resetValue;
+			ImGui::PopStyleColor(3);
+
+			ImGui::SameLine();
+			ImGui::DragFloat("##Y", &values.y, 0.1f);
+			ImGui::PopItemWidth();
+			ImGui::SameLine();
+		}
+
+		{
+			ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.15f,0.2f,0.8f,1.f });
+			ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.2f,0.2f,1.f,1.f });
+			ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.15f,0.2f,0.8f,1.f });
+
+			if (ImGui::Button("Z", buttonSize))
+				values.z = resetValue;
+			ImGui::PopStyleColor(3);
+
+			ImGui::SameLine();
+			ImGui::DragFloat("##Z", &values.z, 0.1f);
+			ImGui::PopItemWidth();
+			ImGui::SameLine();
+		}
+
+		ImGui::PopStyleVar();
+		ImGui::Columns(1);
+		ImGui::PopID();
+	}
 
 	void PropertiesPanel::OnImGuiRender()
 	{
@@ -19,10 +84,10 @@ namespace Sol {
 	}
 	void PropertiesPanel::DrawComponentNodes(Entity entity)
 	{
-		if (entity.HasComponent<NameComp>()) 
+		if (entity.HasComponent<NameComp>())
 		{
 			auto& name = entity.GetComponent<NameComp>().GetName();
-			
+
 			char buffer[256];
 			memset(buffer, 0, sizeof(buffer));
 			strcpy_s(buffer, sizeof(buffer), name.c_str());
@@ -39,16 +104,19 @@ namespace Sol {
 				auto& transform = entity.GetComponent<TransformComp>();
 
 				auto& position = transform.Position;
-				ImGui::DragFloat3("Position", glm::value_ptr(position), 0.25f);
+				DrawVec3Control("Position", position);
+				//ImGui::DragFloat3("Position", glm::value_ptr(position), 0.25f);
 
 				auto& rotation = transform.Rotation;
-				ImGui::DragFloat3("Rotation", glm::value_ptr(rotation), 0.25f);
+				DrawVec3Control("Rotation", rotation);
+				//ImGui::DragFloat3("Rotation", glm::value_ptr(rotation), 0.25f);
 
 				auto& scale = transform.Scale;
-				ImGui::DragFloat3("Scale", glm::value_ptr(scale), 0.25f);
+				DrawVec3Control("Scale", scale, 1.f);
+				//ImGui::DragFloat3("Scale", glm::value_ptr(scale), 0.25f);
 
 				ImGui::TreePop();
-			}	
+			}
 		}
 	}
 }
