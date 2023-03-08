6f5d14bbfbc2fc227401d2d9be8eeceb65ea0a56
M1m1c <M1m1c@users.noreply.github.com>
2023-03-08 21:58:37 +0100

    Added functionfor setting dark theme

--- Sol/Source/Sol/ImGui/ImGuiLayer.cpp
+++ Sol/Source/Sol/ImGui/ImGuiLayer.cpp
@@ -41,11 +41,13 @@ namespace Sol
 		ImGuiStyle& style = ImGui::GetStyle();
 
 		//tweak window rounding and background to look identical to regular ones
-		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) 
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
 		{
 			style.WindowRounding = 0.0f;
 			style.Colors[ImGuiCol_WindowBg].w = 1.0f;
 		}
+
+		SetDarkTheme();
 
 		Application& app = Application::Get();
 		GLFWwindow* window = static_cast<GLFWwindow*>(app.GetWindow().GetNativeWindow());
@@ -76,8 +78,8 @@ namespace Sol
 
 	void ImGuiLayer::OnEvent(Event& e)
 	{
-		if (m_BlockEvents) 
-		{ 
+		if (m_BlockEvents)
+		{
 			ImGuiIO& io = ImGui::GetIO();
 			e.Handled |= e.IsInCategory(EventCategoryMouse) & io.WantCaptureMouse;
 			e.Handled |= e.IsInCategory(EventCategoryKeyboard) & io.WantCaptureKeyboard;
@@ -109,5 +111,79 @@ namespace Sol
 			glfwMakeContextCurrent(backup_Current_Context);
 		}
 	}
-	
+
+	void ImGuiLayer::SetDarkTheme()
+	{
+		auto& colors = ImGui::GetStyle().Colors;
+
+		colors[ImGuiCol_WindowBg] = ImVec4{ 0.113f, 0.113f, 0.109f, 1.0f };
+		colors[ImGuiCol_PopupBg] = ImVec4{ 0.113f, 0.113f, 0.109f, 1.0f };
+		
+		// Headers
+		colors[ImGuiCol_Header] = ImVec4{ 0.25490f,  0.27059f,  0.23529f, 1.0f };
+		colors[ImGuiCol_HeaderHovered] = ImVec4{ 0.39608f,  0.43922f,  0.34510f, 1.0f };
+		colors[ImGuiCol_HeaderActive] = ImVec4{ 0.25490f,  0.27059f,  0.23529f, 1.0f };
+
+		// Buttons
+		colors[ImGuiCol_Button] = ImVec4{ 0.39608f,  0.43922f,  0.34510f, 1.0f };
+		colors[ImGuiCol_ButtonHovered] = ImVec4{ 0.56863f,  0.61961f,  0.51373f, 1.0f };
+		colors[ImGuiCol_ButtonActive] = ImVec4{ 0.39608f,  0.43922f,  0.34510f, 1.0f };
+
+		// Resize Grip
+		colors[ImGuiCol_ResizeGrip] = ImVec4{ 0.18490f,  0.20059f,  0.16529f, 1.0f };
+		colors[ImGuiCol_ResizeGripHovered] = ImVec4{ 0.39608f,  0.43922f,  0.34510f, 1.0f };
+		colors[ImGuiCol_ResizeGripActive] = ImVec4{ 0.56863f,  0.61961f,  0.51373f, 1.0f };
+
+		// Frame BG
+		colors[ImGuiCol_FrameBg] = ImVec4{ 0.18490f,  0.20059f,  0.16529f, 1.0f }; 
+		colors[ImGuiCol_FrameBgHovered] = ImVec4{ 0.39608f,  0.43922f,  0.34510f, 1.0f }; 
+		colors[ImGuiCol_FrameBgActive] = ImVec4{ 0.56863f,  0.61961f,  0.51373f, 1.0f };
+
+		// Tabs
+		colors[ImGuiCol_Tab] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_TabHovered] = ImVec4{ 0.39608f,  0.43922f,  0.34510f, 1.0f };
+		colors[ImGuiCol_TabActive] = ImVec4{ 0.39608f,  0.43922f,  0.34510f, 1.0f };
+		colors[ImGuiCol_TabUnfocused] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_TabUnfocusedActive] = ImVec4{ 0.25490f,  0.27059f,  0.23529f, 1.0f };
+
+		// Title
+		colors[ImGuiCol_TitleBg] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_TitleBgActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_TitleBgCollapsed] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };	
+
+	}
+
+	void ImGuiLayer::SetChernoDarkTheme()
+	{
+		auto& colors = ImGui::GetStyle().Colors;
+		colors[ImGuiCol_WindowBg] = ImVec4{ 0.1f, 0.105f, 0.11f, 1.0f };
+
+		// Headers
+		colors[ImGuiCol_Header] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+		colors[ImGuiCol_HeaderHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
+		colors[ImGuiCol_HeaderActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+
+		// Buttons
+		colors[ImGuiCol_Button] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+		colors[ImGuiCol_ButtonHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
+		colors[ImGuiCol_ButtonActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+
+		// Frame BG
+		colors[ImGuiCol_FrameBg] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+		colors[ImGuiCol_FrameBgHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
+		colors[ImGuiCol_FrameBgActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+
+		// Tabs
+		colors[ImGuiCol_Tab] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_TabHovered] = ImVec4{ 0.38f, 0.3805f, 0.381f, 1.0f };
+		colors[ImGuiCol_TabActive] = ImVec4{ 0.28f, 0.2805f, 0.281f, 1.0f };
+		colors[ImGuiCol_TabUnfocused] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_TabUnfocusedActive] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+
+		// Title
+		colors[ImGuiCol_TitleBg] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_TitleBgActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_TitleBgCollapsed] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+	}
+
 }
