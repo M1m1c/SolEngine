d2462a50dda252b28677e8513535c21d1190cf44
M1m1c <M1m1c@users.noreply.github.com>
2023-03-15 16:09:27 +0100

    Made camera rotation work with mouse, removed button rotation

--- Sol/Source/Sol/CameraController.cpp
+++ Sol/Source/Sol/CameraController.cpp
@@ -23,38 +23,46 @@ namespace Sol
 
 		float dt = deltaTime;
 
+		//TODO normalize direction, lock mouse to view
+
+		Input::SetDisplayCursor(!m_RightMousePressed);
+		if (m_RightMousePressed)
+		{
+			
+			float xDir = m_ViewCenter.x - Input::GetMouseX();
+			float yDir = m_ViewCenter.y - Input::GetMouseY();
+
+			m_CameraTransform.Yaw() += glm::radians(xDir) * m_Sensitivity * dt;
+			m_CameraTransform.Pitch() += glm::radians(yDir) * m_Sensitivity * dt;
+
+			Input::SetMousePosition(m_ViewCenter.x, m_ViewCenter.y);
+			
+		}
+		
+
+		
+
+
 		if (m_DirInputs.any())
 		{
-			auto isCameraPesrpective = m_SceneCamera.Camera.GetIsPerspective();
-			auto forward = isCameraPesrpective ? m_CameraTransform.GetForward() : glm::vec3(0.f, .0f, .0f);
-			auto right = m_CameraTransform.GetRight();
 
-			m_InputAxis.x = m_DirInputs[MoveDir::mBack] - m_DirInputs[MoveDir::mForward];
-			m_InputAxis.y = m_DirInputs[MoveDir::mLeft] - m_DirInputs[MoveDir::mRight];
-			m_InputAxis.z = m_DirInputs[MoveDir::mDown] - m_DirInputs[MoveDir::mUp];
+			
+				//Input Movment
+				auto isCameraPesrpective = m_SceneCamera.Camera.GetIsPerspective();
+				auto forward = isCameraPesrpective ? m_CameraTransform.GetForward() : glm::vec3(0.f, .0f, .0f);
+				auto right = m_CameraTransform.GetRight();
 
-			glm::vec3 dir = glm::normalize((forward * m_InputAxis.x) + (right * m_InputAxis.y) + (WorldUp * m_InputAxis.z));
+				m_InputAxis.x = m_DirInputs[MoveDir::mBack] - m_DirInputs[MoveDir::mForward];
+				m_InputAxis.y = m_DirInputs[MoveDir::mLeft] - m_DirInputs[MoveDir::mRight];
+				m_InputAxis.z = m_DirInputs[MoveDir::mDown] - m_DirInputs[MoveDir::mUp];
 
-			dir = glm::isnan(dir).b ? glm::vec3(0.f) : dir;
-			m_CameraTransform.Position = m_CameraTransform.Position + (dir * m_CameraSpeed * dt);
+				glm::vec3 dir = glm::normalize((forward * m_InputAxis.x) + (right * m_InputAxis.y) + (WorldUp * m_InputAxis.z));
 
-			if (!isCameraPesrpective)
-				m_SceneCamera.Camera.ChangeOrthoSize(m_InputAxis.x * m_CameraSpeed * dt);
-		}
+				dir = glm::isnan(dir).b ? glm::vec3(0.f) : dir;
+				m_CameraTransform.Position = m_CameraTransform.Position + (dir * m_CameraSpeed * dt);
 
-		//Rotation
-
-
-		if (m_RotInputs.any())
-		{
-			float camYawDir = 0;
-			float camPitchDir = 0;
-
-			camYawDir = (m_RotInputs[RotDir::rLeft] * m_RotSpeed) - (m_RotInputs[RotDir::rRight] * m_RotSpeed);
-			camPitchDir = (m_RotInputs[RotDir::rUp] * m_RotSpeed) - (m_RotInputs[RotDir::rDown] * m_RotSpeed);
-
-			m_CameraTransform.Yaw() += glm::radians(camYawDir) * m_Sensitivity * deltaTime;
-			m_CameraTransform.Pitch() += glm::radians(camPitchDir) * m_Sensitivity * deltaTime;
+				if (!isCameraPesrpective)
+					m_SceneCamera.Camera.ChangeOrthoSize(m_InputAxis.x * m_CameraSpeed * dt);
 		}
 
 	}
@@ -91,20 +99,15 @@ namespace Sol
 		m_DirInputs[MoveDir::mUp] = Input::IsKeyPressed(Key::SPACE);
 		m_DirInputs[MoveDir::mDown] = Input::IsKeyPressed(Key::LEFT_SHIFT);
 
-		m_RotInputs[RotDir::rRight] = Input::IsKeyPressed(Key::E);
-		m_RotInputs[RotDir::rLeft] = Input::IsKeyPressed(Key::Q);
-		m_RotInputs[RotDir::rUp] = Input::IsKeyPressed(Key::R);
-		m_RotInputs[RotDir::rDown] = Input::IsKeyPressed(Key::F);
-
-		if (Input::IsKeyPressed(Key::P) && m_OldPserpesctiveInput == false)
+		if (Input::IsKeyPressed(Key::P) && m_OldPerspectiveInput == false)
 		{
-			m_OldPserpesctiveInput = true;
+			m_OldPerspectiveInput = true;
 			m_PerspectiveToggle = !m_PerspectiveToggle;
 			m_SceneCamera.Camera.SetIsPerspective(m_PerspectiveToggle);
 		}
 		else if (!Input::IsKeyPressed(Key::P))
 		{
-			m_OldPserpesctiveInput = false;
+			m_OldPerspectiveInput = false;
 		}
 
 		if (Input::IsKeyPressed(Key::H))
@@ -112,6 +115,17 @@ namespace Sol
 			m_CameraTransform.Position = glm::vec3(0.0f);
 			m_CameraTransform.Rotation = glm::vec3(0.0f);
 		}
+
+
+		m_AltPressed = Input::IsKeyPressed(Key::LEFT_ALT);
+
+		m_RightMousePressed = Input::IsMouseButtonPressed(1);
+		if (m_OldRightMousePressed != m_RightMousePressed && m_RightMousePressed==true) 
+		{
+			Input::SetMousePosition(m_ViewCenter.x, m_ViewCenter.y);
+		}
+		m_OldRightMousePressed = m_RightMousePressed;
+
 	}
 
 }
