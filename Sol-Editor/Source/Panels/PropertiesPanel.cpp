ab74e5b5a41a55dced2f94b7865fc1f4db217d1f
M1m1c <M1m1c@users.noreply.github.com>
2023-03-28 16:44:30 +0200

    added ability to load a new mesh from properties panel

--- Sol-Editor/Source/Panels/PropertiesPanel.cpp
+++ Sol-Editor/Source/Panels/PropertiesPanel.cpp
@@ -2,6 +2,7 @@
 
 #include "PanelUtils.h"
 #include "Sol/Scene/Components.h"
+#include "Sol/Utils/PlatformUtils.h"
 
 #include <glm/gtc/type_ptr.hpp>
 #include <imgui.h>
@@ -78,12 +79,32 @@ namespace Sol
 
 			}, false);
 
-		DrawComponent<ModelComp>("Model", entity, [](ModelComp& component) {
+		auto entityID=entity.GetID();
+		DrawComponent<ModelComp>("Model", entity, [&](ModelComp& component) {
 			ImGui::Columns(2);
 			ImGui::SetColumnWidth(0, 80.f);
-			ImGui::Text("Name");
+			ImGui::Text("Model");
 			ImGui::NextColumn();
-			ImGui::Text(component.ModelPath.c_str());
+
+			if (ImGui::Button(component.ModelPath.c_str())) {
+				std::string filePath = FileDialogs::OpenFile("fbx (*.fbx)\0*.fbx\0");
+				if (!filePath.empty())
+				{
+					auto cleanPath = CleanUpFilePath(filePath);
+					
+					//TODO figure out a cleaner way to do this, since we now do this in the scene as well
+					if(component.ModelPath!=cleanPath)
+					{
+						auto& modelManager = GD_ModelManager::GetInstance();
+
+						GD_Renderer3D::DiscardMeshInstances(entityID, modelManager.GetModel(component.ModelPath));
+						modelManager.DiscardModelInstance(component.ModelPath);
+					}				
+
+					component = ModelComp(cleanPath, entityID);
+					
+				}
+			}
 			ImGui::Columns(1);
 			});
 
@@ -91,5 +112,28 @@ namespace Sol
 			auto& color = component.Color;
 			DrawVec4Control("Color", color, 1.f, 0.01f, 0.f, 1.f, { "R","G","B","A" });
 			});
+	}
+	std::string PropertiesPanel::CleanUpFilePath(std::string& filePath)
+	{
+		std::string cleaned_path; // output path with forward slashes
+
+								  // Find the position of the last backslash in the path
+		size_t last_slash = filePath.find_last_of('\\');
+
+		// Extract the substring starting from the character after the last backslash
+		std::string filename = filePath.substr(last_slash + 1);
+
+		// Find the position of the "assets\\" substring
+		size_t assets_pos = filePath.find("assets\\");
+
+		// Extract the substring starting from the "assets\\" substring
+		std::string directory = filePath.substr(assets_pos);
+
+		// Replace backslashes with forward slashes in the directory string
+		std::replace(directory.begin(), directory.end(), '\\', '/');
+
+		// Concatenate the cleaned up directory and filename strings
+		cleaned_path = directory;
+		return cleaned_path;
 	}
 }
