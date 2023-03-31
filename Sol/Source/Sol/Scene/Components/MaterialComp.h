1ddc8bdad080922b44e89de678695821a8a17351
M1m1c <M1m1c@users.noreply.github.com>
2023-03-31 16:04:51 +0200

    added comments on what needs to be added

--- Sol/Source/Sol/Scene/Components/MaterialComp.h
+++ Sol/Source/Sol/Scene/Components/MaterialComp.h
@@ -1,7 +1,8 @@
 #pragma once
 
+//#include "GalaxyDraw/Interfaces/MaterialManager.h"
+#include "GalaxyDraw/Interfaces/Texture.h"
 #include <glm/glm.hpp>
-#include <GalaxyDraw/Interfaces/Texture.h>
 
 
 namespace Sol
@@ -11,8 +12,21 @@ namespace Sol
 	{
 		glm::vec4 Color{ 1.0f, 1.0f, 1.0f, 1.0f };
 
+		std::string TexturePath;
+
 		MaterialComp() = default;
 		MaterialComp(const MaterialComp&) = default;
+		MaterialComp(const std::string& texturePath) : TexturePath(texturePath) {
+			//TODO ok including material manager causes this to crash, 
+			// so do something similar to model comp and call an interface that can solve thsi for us
+			//m_MaterialIndex = GalaxyDraw::MaterialManager::SetupMaterial(texturePath);
+		}
+
+		uint32_t GetMaterialIndex() { return m_MaterialIndex; }
+		//TODO add set material function that checks with material manager if the material index is valid
+		//TODO add CreateNewMaterialInstance function that asks material manager to create a new material and set it to this
+	private:
+		uint32_t m_MaterialIndex;
 
 	};
 }
