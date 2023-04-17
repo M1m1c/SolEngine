b330c5b10aaa510d0c8492963e93c7638bcd3ab6
M1m1c <M1m1c@users.noreply.github.com>
2023-04-17 17:29:45 +0200

    Added a GetMaterial function

--- Sol/Source/GalaxyDraw/Interfaces/IMaterial.cpp
+++ Sol/Source/GalaxyDraw/Interfaces/IMaterial.cpp
@@ -1,14 +1,21 @@
 #include "solpch.h"
 #include "IMaterial.h"
-
 #include "MaterialManager.h"
 
-uint32_t GalaxyDraw::IMaterial::Create(const std::string& texturePath)
-{
-    return GalaxyDraw::MaterialManager::SetupMaterial(texturePath);
-}
+namespace GalaxyDraw {
 
-uint32_t GalaxyDraw::IMaterial::GetDefaultMaterial()
-{
-    return GalaxyDraw::MaterialManager::GetDefaultMaterial();
+	uint32_t IMaterial::Create(const std::string& texturePath)
+	{
+		return MaterialManager::SetupMaterial(texturePath);
+	}
+
+	uint32_t IMaterial::GetDefaultMaterial()
+	{
+		return MaterialManager::GetDefaultMaterial();
+	}
+
+	std::shared_ptr<Material> IMaterial::GetMaterial(uint32_t materialIndex)
+	{
+		return MaterialManager::GetMaterial(materialIndex);
+	}
 }
--- Sol/Source/GalaxyDraw/Interfaces/IMaterial.h
+++ Sol/Source/GalaxyDraw/Interfaces/IMaterial.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Material.h"
 #include <string>
 
 namespace GalaxyDraw
@@ -8,6 +9,7 @@ namespace GalaxyDraw
 	public:
 		static uint32_t Create(const std::string& texturePath);
 		static uint32_t GetDefaultMaterial();
+		static std::shared_ptr<Material> GetMaterial(uint32_t materialIndex);
 	};
 
 }
