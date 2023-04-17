12b9ea7337da70f9e8b64691da341bcc61e49845
M1m1c <M1m1c@users.noreply.github.com>
2023-04-17 16:28:28 +0200

    moved line down

--- Sol/Source/GalaxyDraw/Interfaces/Material.h
+++ Sol/Source/GalaxyDraw/Interfaces/Material.h
@@ -5,7 +5,8 @@ namespace GalaxyDraw {
 
 	struct Material
 	{
-		Material(uint32_t texIndex,glm::vec4 color=glm::vec4(1.f,1.f,1.f,1.f)): TextureIndex(texIndex),Color(color){}
+		Material(uint32_t texIndex,glm::vec4 color=glm::vec4(1.f,1.f,1.f,1.f)) 
+			: TextureIndex(texIndex),Color(color){}
 		uint32_t TextureIndex;//TextureIndices[5];
 		glm::vec4 Color;
 		// Other material properties go here
