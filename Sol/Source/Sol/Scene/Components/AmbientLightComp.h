db2696754474c94a039a9073674aef295109d25f
M1m1c <M1m1c@users.noreply.github.com>
2023-04-27 14:06:12 +0200

    added constructor

--- Sol/Source/Sol/Scene/Components/AmbientLightComp.h
+++ Sol/Source/Sol/Scene/Components/AmbientLightComp.h
@@ -7,6 +7,11 @@ namespace Sol
 		float Intensity;
 		glm::vec3 AmbientColor;
 
+		AmbientLightComp() {
+			Intensity = 0.f;
+			AmbientColor = glm::vec3(1.f, 1.f, 1.f);
+		}
+
 		AmbientLightComp(float intensity, glm::vec3 ambientColor) 
 			: Intensity(intensity), AmbientColor(ambientColor)
 		{}
